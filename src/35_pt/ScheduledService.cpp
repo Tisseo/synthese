bca607c38b0130aecc1f95e179b7fbe156508ccd
Hugues Romain <hromain@rcsmobility.com>
2013-08-14 19:19:10 +0000

    Bug fixes
    git-svn-id: https://extranet.rcsmobility.com/svn/synthese3/branches/3.9.test1@9272 ae2791e7-e8fc-0310-8f54-ad5a55fdf1de

--- src/35_pt/ScheduledService.cpp
+++ src/35_pt/ScheduledService.cpp
@@ -438,7 +438,7 @@ namespace synthese
 				value.setFromSerializedString(
 					record.get<string>(ScheduledServiceTableSync::COL_DATES)
 				);
-				if(value != *this)
+				if(value.getMarkedDates() != getMarkedDates())
 				{
 					copyDates(value);
 					result = true;
@@ -602,11 +602,6 @@ namespace synthese
 						SchedulesBasedService::DecodeSchedules(
 							_rawSchedule
 					)	);
-					if(	getPath() &&
-						getPath()->getEdges().size() != value.second.size()
-					){
-						throw Exception("Inconsistent schedules size : different from path edges number");
-					}
 					if(	value.first != _departureSchedules ||
 						value.second != _arrivalSchedules
 					){
@@ -615,6 +610,12 @@ namespace synthese
 							value.second,
 							true
 						);
+						if(	getPath() &&
+							(	getPath()->getEdges().size() != _departureSchedules.size() ||
+								getPath()->getEdges().size() != _arrivalSchedules.size()
+						)	){
+							throw Exception("Inconsistent schedules size : different from path edges number");
+						}
 						result = true;
 					}
 				}
